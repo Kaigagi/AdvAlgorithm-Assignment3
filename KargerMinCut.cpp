02e40d316cb25d78af9d42f397231c9b7eb02731
Kaigagi <kaigagipro@gmail.com>
2025-10-14 09:45:44 +1100

    create an edge list and get a random edge from the list then erase that edge

--- KargerMinCut.cpp
+++ KargerMinCut.cpp
@@ -1,10 +1,31 @@
 #include "KargerMinCut.h"
 
+Edge* GetRandomEdgeOnce(vector<Edge*>& allEdges)
+{
+    if (allEdges.empty())
+        return nullptr;
+
+    static random_device rd;
+    static mt19937 gen(rd());
+
+    uniform_int_distribution<> dist(0, allEdges.size() - 1);
+    int index = dist(gen);
+
+    Edge* chosen = allEdges[index];
+
+    // remove it from list so it's not picked again
+    allEdges.erase(allEdges.begin() + index);
+
+    return chosen;
+}
+
 //return the cut flow capacity
 int KargerLoop(FlowGraph& graph){
     //The amount of vertices is the graph vertices at beginning
     int currentVertices = graph.adjacencyList.size();
     UnionFind* uf = new UnionFind(currentVertices);
+
+    vector<Edge*> edgeList
     
     while (currentVertices > 2)
     {

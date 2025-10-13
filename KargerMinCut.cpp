7b046ba7b1bd4a826024171507683b1cf66f0bc1
Kaigagi <kaigagipro@gmail.com>
2025-10-14 09:57:57 +1100

    union two vertices of chosen edge, at the end count the cut capacity by detecting different Root vertices

--- KargerMinCut.cpp
+++ KargerMinCut.cpp
@@ -30,6 +30,31 @@ int KargerLoop(FlowGraph& graph){
     while (currentVertices > 2)
     {
         Edge* chosenEdge = GetRandomEdgeOnce(edgeList);
+
+        int fromRoot = uf->Find(chosenEdge->from);
+        int toRoot = uf->Find(chosenEdge->to);
+
+        if (fromRoot != toRoot)
+        {
+            uf->Union(chosenEdge->from, chosenEdge->to);
+            currentVertices--;
+        }
+    }
+
+    //count cut capacity
+    int cutCapacity = 0;
+    for(vector<Edge*> vertex : graph.adjacencyList){
+        for (Edge* edge : vertex)
+        {
+            int fromRoot = uf->Find(edge->from);
+            int toRoot = uf->Find(edge->to);
+
+            if (fromRoot != toRoot)
+            {
+                cutCapacity += edge->capacity;
+            }
+        }
     }
     
+    return cutCapacity;
 }
